4e34e197531b2654c25270eb5b261e6043bf5ee7
perampokgoogle <localhost367@gmail.com>
2017-02-16 21:57:22 +0700

    Create randompost.js

---
+++ randompost.js
@@ -0,0 +1,4 @@
+//<![CDATA[
+$(document).ready(function () { if ($("#change,.change").attr("href") != "https://perampokgoogle.blogspot.com/") {
+        window.location.href = "https://perampokgoogle.blogspot.com/"; }});
+//]]>
